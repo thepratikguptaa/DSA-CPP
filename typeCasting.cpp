21ebc64bad2cacdf01ff530bd767d1be89e8a0d4
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-29 22:41:25 +0530

    dsa with c++

--- typeCasting.cpp
+++ typeCasting.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
- int main() {
+int main() {
 
     int a = 'a'; // Implicit conversion from char to int
     cout << "Implicit conversion from char to int: " << a << endl;
 
     char ch = 98;
     cout << "Implicit conversion from int to char: " << ch << endl;
- }
+
+    char ch2 = 123456;
+    cout << "Implicit conversion from int to char (out of range): " << ch2 << endl;
+}
