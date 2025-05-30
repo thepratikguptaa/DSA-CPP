a5b6cf968e564e87a105a1837cc3e9fd19c9a855
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-30 10:32:36 +0530

    dsa with c++

--- basics/loops.cpp
+++ basics/loops.cpp
@@ -14,18 +14,27 @@ int main() {
     //     cout << "Both numbers are equal" << endl;
     // }
 
-    char a;
-    cout << "Give me a character: ";
-    cin >> a;
-    cout << "Cool! I got a character now!" << endl;
-    if (a >= 'a' && a <= 'z') {
-        cout << "The given character is a lowercase alphabet letter" << endl;
-    } else if (a >= 'A' && a <= 'Z') {
-        cout << "The given character is an uppercase alphabet letter" << endl;
-    } else if (a>= '0' && a <= '9') {
-        cout << "The given character is a digit" << endl;
-    } else {
-        cout << "The given character is a special character" << endl;
-    }
 
+
+
+
+    // char a;
+    // cout << "Give me a character: ";
+    // cin >> a;
+    // cout << "Cool! I got a character now!" << endl;
+    // if (a >= 'a' && a <= 'z') {
+    //     cout << "The given character is a lowercase alphabet letter" << endl;
+    // } else if (a >= 'A' && a <= 'Z') {
+    //     cout << "The given character is an uppercase alphabet letter" << endl;
+    // } else if (a>= '0' && a <= '9') {
+    //     cout << "The given character is a digit" << endl;
+    // } else {
+    //     cout << "The given character is a special character" << endl;
+    // }
+
+
+
+
+
+    int n;
 }
