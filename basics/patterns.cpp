b800323fd259523226b993e4bed59464384b0db0
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-30 15:02:05 +0530

    dsa with c++

--- basics/patterns.cpp
+++ basics/patterns.cpp
@@ -3,18 +3,34 @@ using namespace std;
 
 int main() {
 
+    // int n;
+    // cout << "Enter the number of rows: "; 
+    // cin >> n; 
+    // int i=1; // Initialize the row counter
+    // while (i<=n) {
+    //     int j=1; // Initialize the column counter
+    //     while(j<=n) {
+    //         cout << "*";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
     int n;
-    cout << "Enter the number of rows: ";
     cin >> n;
-    int i=1; // Initialize the row counter
-    while (i<=n) {
-        int j=1;
-        while(j<=n) {
+    int row = 1;
+    while (row<=n) {
+        int col = 1;
+        while (col <= row) {
             cout << "*";
-            j++;
+            col++;
         }
         cout << endl;
-        i++;
+        row++;
     }
+
+
+
 
 }
