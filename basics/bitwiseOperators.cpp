139a6c878ea6ca3001aa34d862631ce59301a1f9
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-03 17:46:12 +0530

    dsa with c++

---
+++ basics/bitwiseOperators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a=4, b=6;
+
+    cout << "a & b = " << (a & b) << endl; // Bitwise AND
+    cout << "a | b = " << (a | b) << endl; // Bitwise OR
+    cout << "~a = " << (~a) << endl; // Bitwise NOT
+    cout << "a ^ b = " << (a ^ b) << endl; // Bitwise XOR
+
+}
