8658f09c76f9dce25d5c5e3790c7cdd3ca5e8c7c
thepratikguptaa <pratikguptamain@gmail.com>
2025-06-03 18:18:14 +0530

    dsa with c++

--- basics/bitwiseOperators.cpp
+++ basics/bitwiseOperators.cpp
@@ -10,4 +10,7 @@ int main() {
     cout << "~a = " << (~a) << endl; // Bitwise NOT
     cout << "a ^ b = " << (a ^ b) << endl; // Bitwise XOR
 
+    cout << "a << 1 = " << (a << 1) << endl; // Left shift
+    cout << "a >> 1 = " << (a >> 1) << endl; // Right shift
+
 }
