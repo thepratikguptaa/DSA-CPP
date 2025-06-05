910a1d69e0c3d1991c0fb5cc9529adccb7f4c2e3
Pratik Gupta <pratikguptamain@gmail.com>
2025-06-05 23:12:51 +0530

    leetcode problems

--- leetcodeQues/prob-7.cpp
+++ leetcodeQues/prob-7.cpp
@@ -1,3 +1,4 @@
+#include <climits>
 class Solution {
 public:
     int reverse(int x) {
@@ -9,13 +10,14 @@ public:
             // Extract the last digit
             int digit = x % 10;
             
+            // Check for 32-bit integer overflow
+            if (result > INT_MAX/10 || result < INT_MIN/10) {
+                return 0;
+            }
             // Add digit to result after shifting current result
             result = result * 10 + digit;
             
-            // Check for 32-bit integer overflow
-            if (result > 2147483647 || result < -2147483648) {
-                return 0;
-            }
+
             
             // Remove the last digit from x
             x /= 10;
