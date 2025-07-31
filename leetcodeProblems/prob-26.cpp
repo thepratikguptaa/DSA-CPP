36c612e658fe7018282c4c9ee503dd637918cff9
Pratik Gupta <pratikguptamain@gmail.com>
2025-07-31 23:04:12 +0530

    leetcode Remove Duplicates from Sorted Array problem

--- leetcodeProblems/prob-26.cpp
+++ leetcodeProblems/prob-26.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() == 0) return 0;
+        if(nums.empty()) return 0;
         int res = 1;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] != nums[i - 1]){
+        for(size_t i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[res - 1]){
                 nums[res] = nums[i];
                 res++;
             }
@@ -12,3 +17,43 @@ public:
         return res;
     }
 };
+
+// main function to demonstrate the Solution class
+int main() {
+    Solution sol;
+    vector<int> nums1 = {1, 1, 2};
+    
+    cout << "Original vector: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    int k1 = sol.removeDuplicates(nums1);
+
+    cout << "New length: " << k1 << endl;
+    cout << "Vector after removing duplicates: ";
+    for (int i = 0; i < k1; i++) {
+        cout << nums1[i] << " ";
+    }
+    cout << endl << endl;
+
+    vector<int> nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    
+    cout << "Original vector: ";
+    for (int num : nums2) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    int k2 = sol.removeDuplicates(nums2);
+
+    cout << "New length: " << k2 << endl;
+    cout << "Vector after removing duplicates: ";
+    for (int i = 0; i < k2; i++) {
+        cout << nums2[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
