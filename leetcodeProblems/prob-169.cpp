25f55ae76458deb0ece8b29e6767cc9db2dd63c6
Pratik Gupta <pratikguptamain@gmail.com>
2025-08-13 19:49:40 +0530

    another approach for leetcode Majority Element problem

--- leetcodeProblems/prob-169.cpp
+++ leetcodeProblems/prob-169.cpp
@@ -6,3 +6,31 @@ public:
         return nums[n/2];
     }
 };
+
+
+// another approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        //sort
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        //freq count
+        int freq = 1, ans = nums[0];
+        for(int i=1; i<n; i++){
+            if(nums[i] == nums[i-1]){
+                freq++;
+            }
+            else{
+                freq = 1;
+                ans = nums[i];
+            }
+            if(freq > n/2){
+                return ans;
+            }
+        }
+        return ans; // this line is not actually needed, but added for completeness
+    }
+};
