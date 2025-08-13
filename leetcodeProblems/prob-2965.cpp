273c0160a056d5676524cc43ad5c4ce1b54820cb
Pratik Gupta <pratikguptamain@gmail.com>
2025-08-13 22:45:20 +0530

    leetcode Find Missing and Repeated Values problem

---
+++ leetcodeProblems/prob-2965.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int> ans;
+        unordered_set<int> s;
+        int n = grid.size();
+        int a, b;
+
+        int expSum = 0, actualSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                actualSum += grid[i][j];
+                if(s.find(grid[i][j]) != s.end()) {
+                    a = grid[i][j];
+                    ans.push_back(a);
+                } else {
+                    s.insert(grid[i][j]);
+                }
+            }
+        }
+        expSum = (n*n) * (n*n+1) / 2;
+        b = expSum + a - actualSum;
+        ans.push_back(b);
+        return ans;
+    }
+};
